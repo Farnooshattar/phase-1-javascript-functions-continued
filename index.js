93be77e1e616287e6e69b5e68cdd598ee8a209ac
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-03-21 08:34:23 -0400

    complete all

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function wrapAdjective(specilChar="*")
+{
+    return (function(par="special"){return `You are ${specilChar}${par}${specilChar}!`})
+}
+//saturdayFun("bathe my dog")
+function saturdayFun(msg="roller-skate")
+{
+    return `This Saturday, I want to ${msg}!`
+}
+const mondayWork = function(msg="go to the office")
+{
+    return `This Monday, I will ${msg}.`
+}
